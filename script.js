bbd8f634cc1d7b96ae453753477a140cb77aca92
Elad Kuperstock <Eladkuperstock@gmail.com>
2024-05-07 20:12:11 +0300

    Add row divs for the boxes to sit in
    The boxes were entered to the main container, which wouldn't have made them into a grid

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let container = document.getElementById('container');
 
 function makeGrid(x) {
     for (i=0; i<x; i++) {
+        const row = document.createElement('div');
         for (j=0; j<x; j++) {
             const box = document.createElement('div');
             box.classList.add('box');
             box.textContent = i+', '+j;
-            container.appendChild(box);
+            row.appendChild(box);
         }
+        container.appendChild(row);
     }
 }
 

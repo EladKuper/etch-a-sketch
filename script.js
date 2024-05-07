5a3dc0e8c828945ad2305b99ef9907796499fc2c
Elad Kuperstock <Eladkuperstock@gmail.com>
2024-05-07 17:11:31 +0300

    Create a function that creates box divs by num of R and C

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 let container = document.querySelector('.container');
 
+function makeGrid(x) {
+    for (i=0; i<x; i++) {
+        for (j=0; j<x; j++) {
+            const box = document.createElement('div');
+            box.textContent = i+', '+j;
+            container.appendChild(box);
+        }
+    }
+}

0b91fb04759367aab7644cd51f51d9af0db77f16
Elad Kuperstock <Eladkuperstock@gmail.com>
2024-05-07 17:13:49 +0300

    Add a class to every box created in the function

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function makeGrid(x) {
     for (i=0; i<x; i++) {
         for (j=0; j<x; j++) {
             const box = document.createElement('div');
+            box.classList.add('box');
             box.textContent = i+', '+j;
             container.appendChild(box);
         }
     }
 }
+

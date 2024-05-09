d51a23869090339c3b46f6d07439ad91d0166470
Elad Kuperstock <Eladkuperstock@gmail.com>
2024-05-09 19:02:50 +0300

    Add an eventListener to every box

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let container = document.getElementById('container');
 
+function changeColor() {
+    this.style.backgroundColor = "black";
+}
+
 function makeGrid(x) {
     for (i=0; i<x; i++) {
         const row = document.createElement('div');
@@ -7,9 +11,13 @@ function makeGrid(x) {
             const box = document.createElement('div');
             box.classList.add('box');
             row.appendChild(box);
+
+            box.addEventListener("mouseover", changeColor);
         }
         container.appendChild(row);
     }
+    
 }
 
 makeGrid(16);
+    
